cbec4456eed62664485f01a5e3287d1c6be7bae3
jjshko10 <staslukyanenko1995@gmail.com>
2023-05-12 02:08:35 +0300

    create react app

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Routes } from 'react-router-dom';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <span>vdfgergerhehehe</span>
+      <Routes>
+
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
